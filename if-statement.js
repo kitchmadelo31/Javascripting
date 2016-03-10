8e364fb6624482c181f5ab0117c9e743f138335f
Keith Joshua C. Madelo <keithmadelo@gmail.com>
2016-03-10 03:24:42 -0800

    Introduction
    none

--- if-statement.js
+++ if-statement.js
@@ -1,6 +1,5 @@
-var fruit='orange'
+var fruit ='orange';
+fruit=fruit.length;
 if(fruit>5){
-console.log('The fruit name has more than five characters.');
-}else {
-console.log('The fruit name has five characters or less.');
-}
+console.log('The fruit name has more than five characters.');}
+else{console.log('The fruit name has five characters or less');}
